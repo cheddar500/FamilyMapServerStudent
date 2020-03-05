d2dbc57278319c05448d860a7c92cba18e9b59f0
Chandler H <harris.chansey@gmail.com>
2020-03-05 11:55:27 -0700

    Added else clause to RegisterHandler.java

--- src/main/java/fms/Handlers/RegisterHandler.java
+++ src/main/java/fms/Handlers/RegisterHandler.java
@@ -42,11 +42,12 @@ public class RegisterHandler implements HttpHandler {
                 inputExchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
                 RegisterResponse resp = new RegisterResponse("Error: user already exists", false);
                 inputExchange.getResponseBody().write(resp.getResponseBody().getBytes());
+            } else {
+                //send that it was ok
+                inputExchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                //give me where I need to write what happened -> write the response we got
+                inputExchange.getResponseBody().write(response.getResponseBody().getBytes());
             }
-            //send that it was ok
-            inputExchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
-            //give me where I need to write what happened -> write the response we got
-            inputExchange.getResponseBody().write(response.getResponseBody().getBytes());
         }catch(DataAccessException e){
             inputExchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 0);
             RegisterResponse resp = new RegisterResponse(e.getMessage(), false);
